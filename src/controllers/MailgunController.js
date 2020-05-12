cb4b114dca04abbcb17c3200adead31929b9edd1
edsteffen <edineidaniel@gmail.com>
2020-05-11 21:16:59 -0300

    Success Mailgun Implemented
    Great job

--- src/controllers/MailgunController.js
+++ src/controllers/MailgunController.js
@@ -1,6 +1,36 @@
+require('dotenv').config();
+const DOMAIN = process.env.DOMAIN;
+const KEY = process.env.API_KEY;
+const mailgun = require("mailgun-js")({apiKey: KEY, domain: DOMAIN});
 
 module.exports = {
     async send(request, response){
-        return response.status(200).json({ msg: 'RESPONDENDO...'});
+        
+        const data = {
+            from: 'Excited User <me@samples.mailgun.org>',
+            to: 'edineidaniel@gmail.com',
+            subject: 'Hello Edinei',
+            text: 'Yeah, this is Hard!!!'
+        };
+        console.log(DOMAIN);
+        console.log(KEY);
+        console.log(data);
+        mailgun.messages().send(data, function (error, body) {
+            console.log('Estou tenando enviar');
+            console.log(body);
+            console.log('Erro');
+            console.log(error); 
+            return response.json( body ); 
+        });
+    },
+    async domains(request, response){
+        const DOMAIN = process.env.DOMAIN;
+        const KEY = process.env.API_KEY;
+        var mailgun = require('mailgun-js')({ apiKey: KEY, domain: DOMAIN });
+
+        mailgun.get('/domains', function (error, body) {
+            console.log(body);
+            return response.json( {msg:'testando'} ); 
+        });
     }
 }
